602ccceab03e4b3698967a92d6fb7177572b87e4
Rahat-Khan-Pathan <rahatkhanpathan99@gmail.com>
2021-08-18 04:49:55 +0600

    update

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let totalAmount=0;
+
+// To change the input numbers 
 function inputChange(inputId,val,check)
 {
     inputValue = parseInt(document.getElementById(inputId).value);
@@ -6,12 +8,16 @@ function inputChange(inputId,val,check)
     else if(inputValue>=1) inputValue+=val;
     document.getElementById(inputId).value = inputValue;
 }
+
+// To set the value of money 
 function setValue(setId,getId,val)
 {
     value = parseInt(document.getElementById(setId).innerText);
     newValue = val * parseInt(document.getElementById(getId).value);
     document.getElementById(setId).innerText = newValue;
 }
+
+// To set the total money 
 function setSubTotal(id)
 {
     let subTotal=parseInt(document.getElementById('sub-total').innerText);
@@ -21,6 +27,8 @@ function setSubTotal(id)
     else subTotal-=59;
     document.getElementById('sub-total').innerText = subTotal;
 }
+
+// To edit all calculations after removing an item 
 function editCalculations(id)
 {
     let subTotal=0;
@@ -36,17 +44,23 @@ function editCalculations(id)
     setTax();
     setTotal();
 }
+
+// To set tax value 
 function setTax()
 {
     let tax = (parseFloat(document.getElementById('sub-total').innerText)*12)/100;
     document.getElementById('tax').innerText = tax.toFixed(2);
 }
+
+// To set Total amount of money 
 function setTotal()
 {
     let total = parseFloat(document.getElementById('sub-total').innerText) + parseFloat(document.getElementById('tax').innerText);
     document.getElementById('total').innerText = total.toFixed(2);
     totalAmount=total.toFixed(2);
 }
+
+// To remove an item 
 function closeOption(event)
 {
     if(event.target.className == 'remove-item')
@@ -55,6 +69,8 @@ function closeOption(event)
         event.target.parentNode.parentNode.parentNode.parentNode.removeChild(event.target.parentNode.parentNode.parentNode);
     }
 }
+
+// Iphone plus button 
 document.getElementById('iphone-plus').addEventListener('click',function(event){
     inputChange('iphone-input',1,'plus');
     setValue('iphone-value','iphone-input',1219);
@@ -62,6 +78,8 @@ document.getElementById('iphone-plus').addEventListener('click',function(event){
     setTax();
     setTotal();
 });
+
+// Iphone minus button 
 document.getElementById('iphone-minus').addEventListener('click',function(event){
     inputValue = parseInt(document.getElementById('iphone-input').value);
     if(inputValue>0)
@@ -73,6 +91,8 @@ document.getElementById('iphone-minus').addEventListener('click',function(event)
     inputChange('iphone-input',-1,'minus');
     setValue('iphone-value','iphone-input',1219);
 })
+
+// Phone Case plus button 
 document.getElementById('case-plus').addEventListener('click',function(event){
     inputChange('case-input',1,'plus');
     setValue('case-value','case-input',59);
@@ -80,6 +100,8 @@ document.getElementById('case-plus').addEventListener('click',function(event){
     setTax();
     setTotal();
 });
+
+// Phone case minus button 
 document.getElementById('case-minus').addEventListener('click',function(event){
     inputValue = parseInt(document.getElementById('case-input').value);
     if(inputValue>0)
@@ -91,9 +113,13 @@ document.getElementById('case-minus').addEventListener('click',function(event){
     inputChange('case-input',-1,'minus');
     setValue('case-value','case-input',59);
 })
+
+// DOM Bubble to remove an item
 document.getElementById('col').addEventListener('click',function(event){
     closeOption(event);
 })
+
+// Checkout button 
 document.getElementById('checkout').addEventListener('click',function()
 {
     if(totalAmount==0)
